4beac11b647fbc4217c6dfecfc7ac1647ea799ba
KinanthyCn <kinanthycahyaningrum@mail.ugm.ac.id>
2024-03-05 22:10:27 +0700

    lifecycle

--- src/test/java/WalletTest.java
+++ src/test/java/WalletTest.java
@@ -1,17 +1,49 @@
 
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class WalletTest {
     List<Card> empty = new ArrayList<Card>();
     Wallet wallet1 = new Wallet("Android", empty, 0.0);
     Wallet wallet2 = new Wallet("DragonBall", empty, 0.0);
+
+    @BeforeAll
+    void initClass(){
+        empty = new ArrayList<Card>();
+        wallet1 = new Wallet("Android", empty, 0.0);
+        wallet2 = new Wallet("DragonBall", empty, 0.0);
+    }
+
+    @BeforeEach
+    void initMethod(){
+        wallet1.deposit(50000.0);
+        wallet2.deposit(100000.0);
+        wallet1.addCards("BRI", 101);
+        wallet1.addCards("BNI", 202);
+
+    }
+
+    @AfterEach
+    void clearMethod(){
+        wallet1.setCash(0.0);
+        wallet2.setCash(0.0);
+        wallet1.getCards().clear();
+        wallet2.getCards().clear();
+        wallet1.setOwner("Android");
+        wallet2.setOwner("DragonBall");
+    }
+
+    @AfterAll
+    void clearClass(){
+        wallet1 = null;
+        wallet2 = null;
+    }
+
 
     @Test
     void testGetOwner() {
@@ -26,21 +58,17 @@ class WalletTest {
 
     @Test
     void testWithdraw() {
-        wallet1.deposit(10000.0);
-        wallet1.withdraw(5000.0);
-        Assertions.assertEquals(5000.0, wallet1.getCash());
+        wallet1.withdraw(10000.0);
+        Assertions.assertEquals(40000.0, wallet1.getCash());
     }
 
     @Test
     void testDeposit() {
-        wallet1.deposit(5000.0);
-        Assertions.assertEquals(5000.0, wallet1.getCash());
+        Assertions.assertEquals(50000.0, wallet1.getCash());
     }
 
     @Test
     void testAddCards() {
-        wallet1.addCards("BRI", 101);
-        wallet1.addCards("BNI", 202);
         List<Card> cards = new ArrayList<>();
         Card BRI = new Card(wallet1.getOwner(), "101", "BRI");
         cards.add(BRI);
@@ -51,8 +79,6 @@ class WalletTest {
 
     @Test
     void testRemoveCard() {
-        wallet1.addCards("BRI", 101);
-        wallet1.addCards("BNI", 202);
         wallet1.removeCard(202);
         List<Card> cards = new ArrayList<>();
         Card BRI = new Card(wallet1.getOwner(), "101", "BRI");
